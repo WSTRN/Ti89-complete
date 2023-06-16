d5c27c9066e40b41c6bc43105173dce32fad9137
WSTRN <1533225965@qq.com>
2023-06-16 18:52:50 +0800

    add cmpltdat build

--- build.bat
+++ build.bat
@@ -3,9 +3,13 @@
 
 del complete.89z
 del complete.89y
+del cmpltdat.89z
 
 .\a68k.exe complete.asm -g -t -vti89
+.\a68k.exe cmpltdat.asm -g -t -vti89
 .\makeprgm\makeprgm.exe complete
+.\makeprgm\makeprgm.exe cmpltdat
 del complete.o > nul
+del cmpltdat.o > nul
 .\ttppggen.exe complete.89z complete
 
